667779b0a569632439d3858cc2c927337beb5cf5
Mick <Cutipus@protonmail.com>
2023-06-04 02:42:11 +0300

    Graceful quitting with SIGINT as well as input

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 '''Receives commands from user and talks to server'''
 import logging
-from curio import run, run_in_thread, open_connection, spawn, TaskGroup, Queue
+from signal import signal, SIGINT
+from curio import run, run_in_thread, open_connection, spawn, TaskGroup, Queue, Kernel, UniversalEvent
 from socket import SHUT_WR
 
 logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.DEBUG)
@@ -15,41 +16,79 @@ PING_INP = "ping"
 QUIT_INP = "quit"
 HELLO_RSP = "meow"
 
-async def async_input(msg: str = "") -> str:
-    return await run_in_thread(input, msg)
+QUIT = 0
 
-async def process_input(user_input: str):
-    if user_input == HELLO_INP:
-        print(HELLO_RSP)
-    elif user_input == PING_INP:
-        logging.info("pinging . . .")
-        conn = await open_connection(*TRACKER_ADDRESS)
-        await conn.sendall(PING_MSG)
-        await conn.shutdown(SHUT_WR)
-        response = await conn.recv(RECV_SIZE)
-        logging.info(f"Message received: {response}")
-    elif user_input == QUIT_INP:
-        logging.info("Shutting down . . .")
-        raise NotImplemented # implement this with a message queue back to input
     
 
 async def main():
-    input_queue = Queue()
+    user_inputs = Queue()
+    events = Queue()
+    
+    sigint_evt = UniversalEvent()
+    signal(SIGINT, lambda signo, frame: sigint_evt.set())
+
+    async def quit():
+        logging.info("Shutting down . . .")
+
+        await sigint_evt.set()
+        #await events.put(QUIT)
+
+    async def process_input(user_input: str):
+        if user_input == HELLO_INP:
+            print(HELLO_RSP)
+        elif user_input == PING_INP:
+            logging.info("pinging . . .")
+            conn = await open_connection(*TRACKER_ADDRESS)
+            await conn.sendall(PING_MSG)
+            await conn.shutdown(SHUT_WR)
+            response = await conn.recv(RECV_SIZE)
+            logging.info(f"Message received: {response}")
+        elif user_input == QUIT_INP:
+            await quit()
 
     async def input_loop():
         while True:
-            await input_queue.put(await async_input())
+            try:
+                await user_inputs.put(await run_in_thread(input))
+            except EOFError:
+                logging.info("End of input, closing loop")
+                await quit()
+                break
 
     async def process_loop():
         while True:
-            user_input = await input_queue.get()
+            user_input = await user_inputs.get()
             async with TaskGroup() as g:
                 await g.spawn(process_input, user_input)
+
+    async def events_loop():
+        while True:
+            async with TaskGroup() as g:
+                #await g.spawn(events.get)
+                await g.spawn(sigint_evt.wait)
+
+                async for task in g:
+                    if task.name == 'UniversalEvent.wait':
+                        logging.info('wew')
+                        logging.info("Quit msg received; Closing . . .")
+                        return
+
+
+            #if msg == QUIT:
+            #    logging.info("Quit msg received; closing . . .")
+            #    break
 
     async with TaskGroup(wait=any) as g:
         await g.spawn(input_loop)
         await g.spawn(process_loop)
-    
+        await g.spawn(events_loop)
+
 
 if __name__ == '__main__':
     run(main)
+    #kernel = Kernel()
+    #try:
+    #    kernel.run(main())
+    #except KeyboardInterrupt:
+    #    kernel.run(shutdown=True)
+    #    print("OK")
