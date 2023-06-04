08567b6eb7b9b965f68832b2bbf81400579228fe
Mick <Cutipus@protonmail.com>
2023-06-05 01:37:08 +0300

    Refactoring to use a client class, and adding a new auto-ping functionality

--- client.py
+++ client.py
@@ -1,7 +1,8 @@
 '''Receives commands from user and talks to server'''
+# TODO: Transition from using if-else on input types to using StdSignals processing with match case.
 import logging
 from signal import signal, SIGINT
-from curio import run, run_in_thread, open_connection, spawn, TaskGroup, Queue, Kernel, UniversalEvent
+from curio import run, run_in_thread, open_connection, spawn, TaskGroup, Queue, Kernel, UniversalQueue, sleep
 from socket import SHUT_WR
 
 logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.DEBUG)
@@ -13,82 +14,129 @@ PING_MSG = b"ping"
 
 HELLO_INP = "hello"
 PING_INP = "ping"
+STATUS_INP = "status"
 QUIT_INP = "quit"
 HELLO_RSP = "meow"
 
 QUIT = 0
 
-    
 
-async def main():
-    user_inputs = Queue()
-    events = Queue()
-    
-    sigint_evt = UniversalEvent()
-    signal(SIGINT, lambda signo, frame: sigint_evt.set())
+class QuitSignal:
+    pass
 
-    async def quit():
-        logging.info("Shutting down . . .")
 
-        await sigint_evt.set()
-        #await events.put(QUIT)
+class StdInput:
+    __match_args__ = ("input_line", )
+    def __init__(self, input_line: str):
+        self.input_line = input_line
 
-    async def process_input(user_input: str):
+
+class ServerProtocol:
+    def __init__(self, address: tuple[str, int]):
+        self.address = address
+
+    async def ping(self) -> bytes | bool:
+        """Pings the server, returning it's response. If the server is offline returns False"""
+        logging.info("pinging . . .")
+        logging.debug("Opening connection")
+        try:
+            conn = await open_connection(*self.address)
+        except ConnectionRefusedError:
+            logging.debug("Connection from server refused")
+            return False
+        logging.debug("Sending message")
+        await conn.sendall(PING_MSG)
+        await conn.shutdown(SHUT_WR)
+        logging.debug("Message sent")
+        logging.debug("Retrieving response . . .")
+        response = await conn.recv(RECV_SIZE)
+        logging.debug("Responsed retrieved")
+        return response
+
+
+class Client:
+    def __init__(self, address: tuple[str, int]):
+        self.address = address
+        self.server_comm = ServerProtocol(self.address)
+        self.connected = False
+
+        self.signals = UniversalQueue()
+
+
+    async def run(self):
+        signal(SIGINT, lambda signo, frame: self.quit())
+        async with TaskGroup(wait=any) as g:
+            await g.spawn(self.stdinput_loop)
+            await g.spawn(self.events_loop)
+            await g.spawn(self.auto_pinger)
+
+
+    def quit(self):
+        logging.debug("Sending quit signal")
+        self.signals.put(QuitSignal())
+
+
+    async def events_loop(self):
+        """Reads client-related events"""
+        # at the moment this only checks for quit signals
+        while True:
+            signal = await self.signals.get()
+            match signal:
+                case QuitSignal():
+                    logging.info("Quit msg received; Closing . . .")
+                    return
+                case StdInput(user_input): # no longer useful
+                    logging.info("omg: " + str(user_input))
+                case "online":
+                    pass
+                case "offline":
+                    pass
+
+
+    async def auto_pinger(self):
+        """Automatically pings the server every 20 secondes"""
+        while True:
+            if await self.server_comm.ping():
+                logging.info("Ping successful - server online")
+                if self.connected == False:
+                    await self.signals.put("online")
+                self.connected = True
+            else:
+                logging.info("Ping unsuccessful - server offline")
+                if self.connected == True:
+                    await self.signals.put("offline")
+                self.connected = False
+            await sleep(20)
+
+
+    async def process_stdinput(self, user_input: str):
+        """Processes a single line of user input, responding to user commands"""
         if user_input == HELLO_INP:
             print(HELLO_RSP)
+        elif user_input == STATUS_INP:
+            print(self.connected)
         elif user_input == PING_INP:
-            logging.info("pinging . . .")
-            conn = await open_connection(*TRACKER_ADDRESS)
-            await conn.sendall(PING_MSG)
-            await conn.shutdown(SHUT_WR)
-            response = await conn.recv(RECV_SIZE)
-            logging.info(f"Message received: {response}")
+            if response := self.server_comm.ping():
+                print(f"Ping successful! [{response}]")
+            else:
+                print ("Server offline or wrong address")
         elif user_input == QUIT_INP:
-            await quit()
-
-    async def input_loop():
-        while True:
-            try:
-                await user_inputs.put(await run_in_thread(input))
-            except EOFError:
-                logging.info("End of input, closing loop")
-                await quit()
-                break
-
-    async def process_loop():
-        while True:
-            user_input = await user_inputs.get()
-            async with TaskGroup() as g:
-                await g.spawn(process_input, user_input)
-
-    async def events_loop():
-        while True:
-            async with TaskGroup() as g:
-                #await g.spawn(events.get)
-                await g.spawn(sigint_evt.wait)
-
-                async for task in g:
-                    if task.name == 'UniversalEvent.wait':
-                        logging.info('wew')
-                        logging.info("Quit msg received; Closing . . .")
-                        return
+            quit()
 
 
-            #if msg == QUIT:
-            #    logging.info("Quit msg received; closing . . .")
-            #    break
+    async def stdinput_loop(self):
+        """Indefinitely reads user input from a different thread and processes it"""
+        async with TaskGroup() as g:
+            while True:
+                try:
+                    user_input = await run_in_thread(input)
+                    await g.spawn(self.process_stdinput, user_input)
+                except EOFError:
+                    logging.debug("End of input")
 
-    async with TaskGroup(wait=any) as g:
-        await g.spawn(input_loop)
-        await g.spawn(process_loop)
-        await g.spawn(events_loop)
-
+async def main():
+    client = Client(TRACKER_ADDRESS)
+    await client.run()
 
 if __name__ == '__main__':
     run(main)
-    #kernel = Kernel()
-    #try:
-    #    kernel.run(main())
-    #except KeyboardInterrupt:
-    #    kernel.run(shutdown=True)
-    #    print("OK")
